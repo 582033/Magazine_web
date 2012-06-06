da69447e431a0e64dc66f1f74edea52d128b8247
Meng Tong <mtong@eee168.com>
2012-06-06 19:05:02 +0800

    magazine.php数据获取临时提交

--- application/controllers/magazine.php
+++ application/controllers/magazine.php
@@ -56,4 +56,52 @@ class Magazine extends MY_Controller {
 		//$this->smarty->view('index.tpl');
 	}	//}}}
 	
+		$mag_list = api("http://api.1001s.cn/magazine/mag_list?start=".$gets['start']."&limit=".$gets['limit']);
+		$this->_json_output($mag_list['data']);
+	}	//}}}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	function loved_num(){		//喜欢数量{{{
+		$loved_num = api("http://mtong.api.1001s.cn/magazine/get_loved_nums");
+		$this->_json_output($loved_num['data']);
+	}//}}}
+	
+	function loved_data(){		//喜欢数据{{{
+		$keys = array('start', 'limit');
+		$gets = $this->_get_more_non_empty($keys);
+		$type = $this->input->get('type');
+		$loved_data = api("http://mtong.api.1001s.cn/magazine/get_loved_data?limit=".$gets['limit']."&start=".$gets['start']."&type=".$type);
+		$this->_json_output($loved_data['data']);
+	}//}}}
+	
+	function user_comment(){		//获得评论{{{
+		$keys = array('start', 'limit');
+		$gets = $this->_get_more_non_empty($keys);
+		$magazine_id = $this->input->get('magazine_id');
+		$user_comment = api("http://mtong.api.1001s.cn/magazine/get_user_comment?limit=".$gets['limit']."&start=".$gets['start']."&magazine_id=".$magazine_id);
+		$this->_json_output($user_comment['data']);
+	}//}}}
+	
+	function mag_element(){			//杂志元素{{{
+		$keys = array('for', 'start', 'limit');
+		$gets = $this->_get_more_non_empty($keys);
+		$type = $this->input->get('type');
+		$mag_element = api("http://api.1001s.cn/magazine/get_mag_element?for=".$gets['for']."&limit=".$gets['limit']."&start=".$gets['start']."&type=".$type);
+		$this->_json_output($mag_element['data']);
+	}//}}}
+	
+	function detail(){		//杂志详情{{{
+		$id = $this->_get_non_empty('id');
+		$mag_info = api("http://api.1001s.cn/magazine/detail?id=".$id);
+		$this->_json_output($mag_info['data']);
+	}//}}}
 }
