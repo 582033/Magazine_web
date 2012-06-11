0f1341c610b1417be939ee699053d4af1f80bfeb
yjiang <yjiang@eee168.com>
2012-06-11 10:12:56 +0800

    增加登录

--- application/controllers/magazine.php
+++ application/controllers/magazine.php
@@ -7,14 +7,34 @@ class Magazine extends MY_Controller {
 		parent::__construct();
 		$this->load->helper('api');
 		$this->api_host = $this->config->item('api_host');
+		$this->load->model('Login_Model');	
 	}
-
+	
+	function _get_more ($keys, $input){	//{{{		
+		$return = array();
+		foreach ($keys as $key){
+			$return[$key] =	$this->input->$input($key);
+		}
+		return $return;
+	}	//}}}
+	
 	function _get_more_non_empty($more) {	//{{{
 		$return = array();
 		foreach ($more as $item) {
 			$return[$item] = $this->_get_non_empty($item);
 		}
 		return $return;
+	}	//}}}
+
+	function login (){	//{{{
+	//	$username = $this->input->post('username');
+	//	$passwd = $this->input->post('passwd');
+		$username = $this->input->get('username');
+		$passwd = $this->input->get('passwd');
+		$getkey = api($this->api_host."/magazine/getkey");	
+		echo "<pre>";print_r($getkey);
+		$return = $this->Login_Model->login($getkey, $username, $passwd);
+		echo "<pre>";print_r($return);
 	}	//}}}
 
 	function mag_list (){	//杂志列表{{{
@@ -25,7 +45,7 @@ class Magazine extends MY_Controller {
 		//$this->smarty->view('index.tpl');
 	}	//}}}
 
-	function detail (){	//{{{
+	function detail (){	//详情页`{{{
 		$id = $this->_get_non_empty('id');		
 		$detail = api($this->api_host."/magazine/detail?id=".$id);
 		echo "<pre>";print_r($detail);
