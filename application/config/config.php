d037c7b8a6882efd75d69d25ec71f54a64f1fef7
yjiang <yjiang@eee168.com>
2012-07-02 22:05:51 +0800

    修改ａｐｉ的host

--- application/config/config.php
+++ application/config/config.php
@@ -401,7 +401,7 @@ $config['mq_user'] = 'guest';
 $config['mq_pass'] = 'guest';
 
 
-$config['api_host'] = 'http://api.in1001.com';
+$config['api_host'] = 'http://api.in1001.com/v1';
 $config['web_host'] = 'http://www.in1001.com';
 
 $config['cookie_expire'] = '1209600';//cookie缓存时间,14天
